7439cd9b769f2815469e8e967c6179c05d761190
mohamedhibet16-blip <mohamedhibet16@gmail.com>
2026-04-12 22:03:23 +0300

    Create sqrt.py

---
+++ sqrt.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 0, x
+        answer = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if mid * mid <= x:
+                answer = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return answer
